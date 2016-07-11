c9ed9fc1ae2343527223ecaad70ac1b852ae81dd
mh-cbon <mh-cbon@users.noreply.github.com>
2016-07-11 22:37:33 +0200

    imporove installer

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
+REPO="go-bin-deb"
 ARCH=$(uname -m)
 case $ARCH in
 	arm*) ARCH="arm";;
 	x86) ARCH="386";;
 	x86_64) ARCH="amd64";;
 esac
-latest=`wget -q --no-check-certificate -O - https://api.github.com/repos/mh-cbon/go-bin-deb/releases/latest | grep -E '"tag_name": "([^"]+)"' | cut -d '"' -f4`
-wget --no-check-certificate https://github.com/mh-cbon/go-bin-deb/releases/download/${latest}/go-bin-deb-${ARCH}.deb
-sudo dpkg -i go-bin-deb-${ARCH}.deb
+latest=`wget -q --no-check-certificate -O - https://api.github.com/repos/mh-cbon/${REPO}/releases/latest | grep -E '"tag_name": "([^"]+)"' | cut -d '"' -f4`
+wget --no-check-certificate https://github.com/mh-cbon/${REPO}/releases/download/${latest}/${REPO}-${ARCH}.deb
+sudo dpkg -i ${REPO}-${ARCH}.deb
