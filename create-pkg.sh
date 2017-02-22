196c324255492e9e5be4b6a4210a03fef78e048c
mh-cbon <mh-cbon@users.noreply.github.com>
2017-02-22 21:46:03 +0100

    createpkg script: add tree

--- create-pkg.sh
+++ create-pkg.sh
@@ -28,7 +28,7 @@ fi
 rm -fr pkg-build/*
 mkdir -p pkg-build/{386,amd64}
 
-tree
+ls -al .
 
 go-bin-deb generate -a 386 --version ${TRAVIS_TAG} -w pkg-build/386/ -o ${TRAVIS_BUILD_DIR}/${REPO}-386.deb
 go-bin-deb generate -a amd64 --version ${TRAVIS_TAG} -w pkg-build/amd64/ -o ${TRAVIS_BUILD_DIR}/${REPO}-amd64.deb
