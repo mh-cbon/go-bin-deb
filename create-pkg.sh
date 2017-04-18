4504b3f7a6c92bf42398d5b7cb467d00bd12bd35
mh-cbon <mh-cbon@users.noreply.github.com>
2017-04-18 21:39:56 +0200

    support: updated all support script

--- create-pkg.sh
+++ create-pkg.sh
@@ -9,8 +9,12 @@
 # | GH=YOUR/REPO sh -xe
 
 if [ "${GH}" = "mh-cbon/go-bin-deb" ]; then
+  oldp=`pwd`
   git pull origin master
   git checkout -b master
+  cd ..
+  cd $oldp
+
     # go get -u github.com/mh-cbon/go-bin-deb/go-bin-deb-utils
   # go run go-bin-deb-utils/*go create-packages -repo=$GH
 # else
