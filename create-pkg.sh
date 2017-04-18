bb0fa406a610a79837110e20e3bb07d01ad23bfb
mh-cbon <mh-cbon@users.noreply.github.com>
2017-04-18 19:49:42 +0200

    support: updated all support script

--- create-pkg.sh
+++ create-pkg.sh
@@ -8,12 +8,12 @@
 # curl -L https://raw.githubusercontent.com/mh-cbon/go-bin-deb/master/create-pkg.sh \
 # | GH=YOUR/REPO sh -xe
 
-if ["${GH}" == "mh-cbon/go-bin-deb"]; then
+if [ "${GH}" = "mh-cbon/go-bin-deb" ]; then
   git checkout -b master
   git pull remote
   ls -al
   go run go-bin-deb-utils/*go create-packages -repo=$GH
 else
-go get -u github.com/mh-cbon/go-bin-deb/go-bin-deb-utils
-go-bin-deb-utils create-packages -repo=$GH
+  go get -u github.com/mh-cbon/go-bin-deb/go-bin-deb-utils
+  go-bin-deb-utils create-packages -repo=$GH
 fi
